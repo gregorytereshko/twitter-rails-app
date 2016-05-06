6513dd99c1623ab157a247176d27af05130bff9f
gregorytereshko <gregory.tereshko1@gmail.com>
2016-05-06 22:26:06 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   # get 'static_pages/home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
 
   root 'static_pages#home'
+  get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
