19048efa43cc9d37e17bff9b4294ccb743eca81e
gregorytereshko <gregory.tereshko1@gmail.com>
2016-05-11 22:14:12 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Gregory Tereshko'
+    email 'gregory.tereshko1@gmail.com'
+    password 'foobar'
+    password_confirmation 'foobar'
+
+  end
+end
